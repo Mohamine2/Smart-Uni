0a75e2fb6ae9cdc322b8759c858aa04bb8993701
Mohamine2 <mohamed.bouazzaoui093@gmail.com>
2026-04-07 23:06:20 +0200

    Organisation des fichiers html en projet Django structuré et implémentation des requêtes de connexion

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from residence_connectee import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home_view, name='home'),
+    path('index/', views.home_view, name='home'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('dashboard-simple/', views.dashboard_view, name='dashboard'), # Page après connexion
 ]
--- residence_connectee/views.py
+++ residence_connectee/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def home_view(request):
+    return render(request, 'index.html')
+
+def register_view(request):
+    return render(request, 'register.html')
+    
+def login_view(request):
+    if request.method == 'POST':
+        user_nom = request.POST.get('username')
+        user_mdp = request.POST.get('password')
+
+        # authenticate vérifie le hash automatiquement
+        user = authenticate(request, username=user_nom, password=user_mdp)
+
+        if user is not None:
+            login(request, user) # Crée la session
+            return redirect('dashboard') # Redirige vers une page de succès
+        else:
+            messages.error(request, "Identifiants invalides.")
+    
+    return render(request, 'login.html')
+
+@login_required  # Cette ligne empêche les gens non-connectés d'entrer
+def dashboard_view(request):
+    # Ici, request.user contient l'étudiant qui vient de se connecter
+    return render(request, 'dashboard-simple.html', {'etudiant': request.user})
