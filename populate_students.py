94f49c0ee8b901b61c5cec2323c10aea37b7e94d
Mohamine2 <mohamed.bouazzaoui093@gmail.com>
2026-04-07 20:01:39 +0200

    Ajout fonction de peuplage étudiants pour la DB

---
+++ populate_students.py
@@ -0,0 +1,59 @@
+import os
+import django
+from faker import Faker
+import random
+import string
+
+# Configuration de l'environnement Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django.setup()
+
+from residence_connectee.models import Etudiant 
+
+fake = Faker('fr_FR') # On utilise la version française pour des noms cohérents
+
+def generate_random_password(length=12):
+    """Génère un mot de passe robuste mélangeant lettres et chiffres"""
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choice(characters) for i in range(length))
+
+def populate(n=20):
+    for i in range(n):
+        # Génération des données de base
+        first_name = fake.first_name()
+        last_name = fake.last_name()
+        
+        # Création d'un username unique (ex: jdupont_42)
+        username = f"{first_name[0].lower()}{last_name.lower()}_{random.randint(10, 99)}"
+        email = f"{username}@student.cytech.fr"
+        
+        # Numéro de téléphonee
+        phone = fake.phone_number()
+
+        raw_password = generate_random_password()
+
+        # On utilise get_or_create pour ne pas recréer les mêmes si on relance
+        user, created = Etudiant.objects.get_or_create(
+            username=username,
+            defaults={
+                'first_name': first_name,
+                'last_name': last_name,
+                'email': email,
+                'telephone': phone,
+                'is_active': True,
+            }
+        )
+
+        if created:
+            # IMPORTANT : On définit un mot de passe par défaut
+            # Django va le HASHER automatiquement ici
+            user.set_password(raw_password)
+            user.save()
+            print(f"Étudiant créé : {username} | Tel : {phone}")
+        else:
+            print(f"L'étudiant {username} existe déjà.")
+
+if __name__ == '__main__':
+    print("Début du peuplement...")
+    populate(20)
+    print("Opération terminée avec succès !")
--- residence_connectee/models.py
+++ residence_connectee/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Etudiant(AbstractUser):
     # Les champs nom, prenom, email et password sont déjà inclus dans AbstractUser
     # On ajoute uniquement le téléphone
-    telephone = models.CharField(max_length=15, blank=True, null=True)
+    telephone = models.CharField(max_length=30, blank=True, null=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.email})"
